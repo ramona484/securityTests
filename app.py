af6c81d2887f16a7a81e4f9f64681073dd28ca0d
ramona484 <70355247+ramona484@users.noreply.github.com>
2021-01-23 05:30:58 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello():
+    return "Hello World! Hello World!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
