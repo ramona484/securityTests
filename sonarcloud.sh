10f74476ef13db9a021839e57a046cd3636165b6
Ramona84 <ramona.rettig@outlook.de>
2021-01-25 20:42:51 +0100

    add sonarcloud

--- sonarcloud.sh
+++
@@ -1,8 +0,0 @@
-addons:
-  sonarcloud:
-    organization: ramona484 # the key of the org you chose at step #3
-    token:
-      secure: $SONAR_TOKEN # encrypted value of your token
-script:
-  # other script steps might be done before running the actual analysis
-  - sonar-scanner
