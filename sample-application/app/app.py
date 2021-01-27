b1e018236c0cc104b665ec6ecbd5a5c294a2cab7
Ramona84 <ramona.rettig@outlook.de>
2021-01-27 22:29:52 +0100

    add trivvy

--- sample-application/app/app.py
+++ sample-application/app/app.py
@@ -6,6 +6,8 @@ CORS(app)
 
 @app.route("/")
 def hello():
-    return "Hello World! Hello World! Hello World! Hello World!"
+    return "Hello World! Hello World! Hello World! Hello World! Hello World!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
+
+
